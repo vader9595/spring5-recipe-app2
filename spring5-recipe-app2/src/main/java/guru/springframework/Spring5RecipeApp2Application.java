97de54493af81db4aa66862dac1f368a07f94d58
vader9595 <102444679+vader9595@users.noreply.github.com>
2022-05-24 15:54:40 -0500

    Initial commit

---
+++ spring5-recipe-app2/src/main/java/guru/springframework/Spring5RecipeApp2Application.java
@@ -0,0 +1,19 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipeApp2Application.class, args);
+
+		for(int i = 0; i<10;i++) {
+			System.out.println("nick245999990000011111111");
+		}
+	}
+
+
+
+}
---
+++ spring5-recipe-app2/src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,50 @@
+package guru.springframework.domain;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+
+
+
+    //Below are just Getters and Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
